21a9e01bbe832d1e9957a32051b5b31da2b44dce
VirgoPR <90396302+VirgoPR@users.noreply.github.com>
2022-06-22 08:59:28 -0700

    Contact page

---
+++ c-index.js
@@ -0,0 +1,76 @@
+
+ var btn = document.getElementById('btn');
+ btn.addEventListener ('click', function (e) {
+    e.preventDefault()
+    var name = document.getElementById('name').value;
+    var subject = document.getElementById('subject').value;
+    var phone = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    var body = 'name: ' + name + '<br/> email : ' + email + '<br/> subject : ' + subject + ''
+
+    Email.send({
+    Host : "gmail.com",
+    Username : "poojaranjani58@gmail.com",
+    Password : "ynulejsmnhaxzpfo",
+    To : 'poojaranjani58@gmail.com',
+    From : email,
+    Subject :  subject,
+    Body : body,
+    }).then(
+  message => alert(message)
+);
+
+
+
+
+// function validate(){
+//     var name = document.getElementById("name").value;
+//     var subject = document.getElementById("subject").value;
+//     var phone = document.getElementById("phone").value;
+//     var email = document.getElementById("email").value;
+//     var message = document.getElementById("message").value;
+//     var error_message = document.getElementById("error_message");
+    
+//     error_message.style.padding = "10px";
+    
+//     var text;
+//     if(name.length < 5){
+//       text = "Please Enter valid Name";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     if(subject.length < 10){
+//       text = "Please Enter Correct Subject";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     if(isNaN(phone) || phone.length != 10){
+//       text = "Please Enter valid Phone Number";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     if(email.indexOf("@") == -1 || email.length < 6){
+//       text = "Please Enter valid Email";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     if(message.length <= 140){
+//       text = "Please Enter More Than 140 Characters";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+
+//             Email.send({
+//         Host : "gmail.com",
+//         Username : "poojaranjani58@gmail.com",
+//         Password : "ynulejsmnhaxzpfo",
+//         To : 'poojaranjani58@gmail.com',
+//         From : email,
+//         Subject : subject,
+//         Body : body,
+//     }).then(
+//       message => alert(message)
+//     );
+    
+//   }
